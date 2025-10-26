7dd9d258de0ff6bdb00ce6087d0a3c39e7b8b954
Dhimaan Dutta <dhimaanmams.07@gmail.com>
2025-10-26 19:31:28 +0000

    Update chatbot_chain.py

--- backend/services/chatbot_chain.py
+++ backend/services/chatbot_chain.py
@@ -5,6 +5,6 @@ from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 
 load_dotenv()
-llm = ChatCohere(model="command-xlarge-nightly", cohere_api_key="W9T9D3DGjtqAEgPEAJlr0J8GWYMLDwSNm4EqYi3Y")
+llm = ChatCohere(model="command-xlarge-nightly", cohere_api_key="-")
 memory = ConversationBufferMemory()
 chatbot_chain = ConversationChain(llm=llm, memory=memory)
