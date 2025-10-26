24dcbdab2526c4503837b4053f6a66a3eada30cb
Dhimaan Dutta <dhimaanmams.07@gmail.com>
2025-10-26 19:30:57 +0000

    Update generator_chain.py

--- backend/services/generator_chain.py
+++ backend/services/generator_chain.py
@@ -5,7 +5,7 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
 load_dotenv()
-llm = ChatCohere(model="command-xlarge-nightly", cohere_api_key="W9T9D3DGjtqAEgPEAJlr0J8GWYMLDwSNm4EqYi3Y")
+llm = ChatCohere(model="command-xlarge-nightly", cohere_api_key="-")
 template = "You are an academic writer. Generate a {type} on: {topic}"
 prompt = PromptTemplate(input_variables=["type", "topic"], template=template)
 generator_chain = LLMChain(llm=llm, prompt=prompt)
